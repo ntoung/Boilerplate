3787c99162df93098035e1fa7072ad57eaf8aa68
ykotturi <ykotturi@ucsd.edu>
2014-04-18 19:07:18 -0700

    login.handlebars update

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var http = require('http');
 var path = require('path');
 var handlebars = require('express3-handlebars');
 var app = express();
+var graph = require('fbgraph');
+
 
 //route files to load
 var index = require('./routes/index');
